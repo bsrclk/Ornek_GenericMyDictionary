af893227d774fd7cac4522ded75f20bab7bf10bb
ÇOLAK <OLAK@DESKTOP-8FPMOJU>
2021-08-02 15:33:39 +0300

    Add project files.

---
+++ Ornek_GenericMyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ornek_GenericMyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] _MyList;
+        T[] _TempMyList;
+        public MyDictionary()
+        {
+
+            _MyList = new T[0];
+
+        }
+
+
+
+
+
+
+        public void Add(T BenimTipim)
+        {
+            _TempMyList = _MyList;
+            _MyList = new T[_MyList.Length + 1];
+
+            for (int i = 0; i < _TempMyList.Length; i++)
+            {
+                _MyList[i] = _TempMyList[i];
+            }
+
+            _MyList[_MyList.Length - 1] = BenimTipim;
+
+        }
+        
+        
+        
+    
+    
+         public int Count
+        {
+            get { return _MyList.Length; }
+       
+        }
+    
+    
+    
+    
+    }
+}
---
+++ Ornek_GenericMyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ornek_GenericMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> Secenekler = new MyDictionary<string>();
+            Secenekler.Add("HAYATI DOLU DOLU YAŞAMAK");
+            Secenekler.Add("HAYATI DOLU DOLU YAŞAMAK");
+            Secenekler.Add("HAYATI DOLU DOLU YAŞAMAK");
+            Console.WriteLine(Secenekler.Count);
+        }
+    }
+}
